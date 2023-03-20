605ca3d74358ca2f1445e8d141e8e7af915d9d89
Donggeun <ezfood3@gmail.com>
2023-03-20 14:04:36 +0900

    deep_learning test

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ for i, rows in data.iterrows():
 yData = data['admit'].values
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(64, activation='tanh'),
     tf.keras.layers.Dense(128, activation='tanh'),
     tf.keras.layers.Dense(256, activation='tanh'),
+    tf.keras.layers.Dense(512, activation='tanh'),
+    tf.keras.layers.Dense(1024, activation='tanh'),
     tf.keras.layers.Dense(1, activation='sigmoid'),
 ])
 
@@ -25,5 +26,5 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 
 model.fit(np.array(xData), np.array(yData), epochs=1000)
 
-result = model.predict([[750, 3.70, 3], [400, 2.2, 1]])
+result = model.predict([[750, 3.70, 3], [400, 2.2, 1], [800, 4.5, 4]])
 print(result)
